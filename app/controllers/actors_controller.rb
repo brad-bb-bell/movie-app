5a37d99ec155a2a33a5065ae178763e24d87b154
Brad Bell <brad.bb.bell@gmail.com>
2022-05-09 20:31:57 -0600

    Adds all RESTful actions and routes and uupdates methods in Movies and Actors controllers

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,8 +1,38 @@
 class ActorsController < ApplicationController
-  def show
-    actor = Actor.find_by(id: params[:id])
+  def index
+    actor = Actor.all
     render json: actor.as_json
   end
-end
 
+  def show
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    render json: actor.as_json
+  end
 
+  def create
+    actor = Actor.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+    )
+    actor.save
+    render json: actor.as_json
+  end
+
+  def update
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    actor.first_name = params["first_name"] || actor.first_name
+    actor.last_name = params["last_name"] || actor.last_name
+    actor.known_for = params["known_for"] || actor.known_for
+    actor.save
+    render json: actor.as_json
+  end
+
+  def destroy
+    actor_id = params[:id]
+    actor = Actor.find_by(id: actor_id)
+    actor.destroy
+    render json: { message: "Actor deleted." }
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,28 @@
 actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
 actor.save
 Movie.create(title: "Top Gun", year: 1992, plot: "Maverick and Goose fly American fighter planes and (*spoiler alert*) Goose dies")
+
+# {
+#   "id": 1,
+#   "title": "Avengers: Endgame",
+#   "year": 2019,
+#   "plot": "Thanos tries to get all the infinity stones so he can snap his fingers",
+#   "created_at": "2022-05-06T14:33:39.684Z",
+#   "updated_at": "2022-05-06T14:33:39.684Z"
+# },
+# {
+#   "id": 2,
+#   "title": "Encanto",
+#   "year": 2021,
+#   "plot": "We don't talk about Bruno no no",
+#   "created_at": "2022-05-06T14:34:33.180Z",
+#   "updated_at": "2022-05-06T14:34:33.180Z"
+# },
+# {
+#   "id": 3,
+#   "title": "The Dawn Wall",
+#   "year": 2018,
+#   "plot": "Tommy Caldwell climbs the impossible Dawn Wall on El Cap in Yosemite National Park",
+#   "created_at": "2022-05-06T14:35:37.173Z",
+#   "updated_at": "2022-05-06T14:35:37.173Z"
+# },
